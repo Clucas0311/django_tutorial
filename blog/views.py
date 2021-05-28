8f0e9748b23aad5f2f3a0a0a168ec34f28e047fb
Charles <clucas0311@gmail.com>
2021-05-28 17:31:32 -0400

    Feat: added datetime and imported data

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,5 @@
 from django.shortcuts import render
 from .models import Post
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'CharlesL',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
 
 # Create your views here.
 def home(request):
